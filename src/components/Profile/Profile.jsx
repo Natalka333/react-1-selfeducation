4b461c07536e5ab1ceec9bd4e14f783e1b0ca833
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2024-07-12 17:30:20 +0300

    add icon

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Container, Description, UserName, Tag, Avatar, Location, Stats, StatsItem, Label, Quantity } from '../Profile/Profile.styled';
-import { FaUserAlt, FaLocationDot } from 'react-icons/fa';
+import { FaUserAlt, FaMapMarkerAlt, FaTags } from 'react-icons/fa';
 
 export default function Profile({ username, tag, location, avatar, stats }) {
     return (
@@ -11,9 +11,8 @@ export default function Profile({ username, tag, location, avatar, stats }) {
                     alt={username} 
                 />
                 <UserName><FaUserAlt size={16}/>  {username}</UserName>
-                
-                <Tag>{tag}</Tag>
-                <Location> <FaLocationDot size={16}/>  {location}</Location>
+                <Tag><FaTags size={16}/>  {tag}</Tag>
+                <Location><FaMapMarkerAlt size={16}/>  {location}</Location>
             </Description>
 
             <Stats>
